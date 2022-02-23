804e3cb911b8c8cd8de51ecb22bcd1043328147a
seregious <seregious@gmail.com>
2022-02-23 23:54:23 +0300

    Update

--- Bootcamp/Bootcamp/Intermediate 16-29/PublishersAndSubscribers.swift
+++ Bootcamp/Bootcamp/Intermediate 16-29/PublishersAndSubscribers.swift
@@ -10,20 +10,41 @@ import Combine
 
 class SubscriberViewModel: ObservableObject {
     
-    @Published var count = 0
-    var timer: AnyCancellable?
+    @Published var count: Int = 0
+    @Published var text = ""
+    @Published var textIsValid = false
+    var cancellables = Set<AnyCancellable>()
     
     init() {
         setUpTimer()
+        addTextFieldSubriber()
+    }
+    
+    func addTextFieldSubriber() {
+        $text
+            .map { (text) -> Bool in
+                if text.count > 3 {
+                    return true
+                }
+                return false
+            }
+            //.assign(to: \.textIsValid, on: self)
+            .sink(receiveValue: { [weak self] (isValid) in
+                self?.textIsValid = isValid
+            })
+            .store(in: &cancellables)
     }
     
     func setUpTimer() {
-        timer = Timer
+        Timer
             .publish(every: 1, on: .main, in: .common)
             .autoconnect()
-            .sink {[weak self] _ in
-                self?.count += 1
+            .sink { [weak self] _ in
+                guard let self = self else {return}
+                self.count += 1
+                
             }
+            .store(in: &cancellables)
     }
 }
 
@@ -35,8 +56,14 @@ struct PublishersAndSubscribers: View {
         VStack {
             Text("\(vm.count)")
                 .font(.largeTitle)
+            
+            TextField("Type something here", text: $vm.text)
+                .frame(height: 55)
+                .font(.headline)
+                .background(.gray.opacity(0.3))
+                .cornerRadius(25)
         }
-       
+        .padding()
     }
 }
 
